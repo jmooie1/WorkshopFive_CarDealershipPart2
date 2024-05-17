68d8f98d5b31948338c940935ed4ed8a08c931c3
Jason <jmdestiny13@gmail.com>
2024-05-17 11:55:17 -0400

    first commit

---
+++ src/main/java/com/ps/Main.java
@@ -0,0 +1,11 @@
+package com.ps;
+
+public class Main {
+    public static void main(String[] args) {
+        //This creates an instance of UserInterface to manage all the user interactions.
+        UserInterface UI = new UserInterface();
+
+        // This displays the user interface and handles all user commands.
+        UI.display();
+    }
+}
